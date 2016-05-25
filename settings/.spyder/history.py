0aaa449684663f12fd26de978a6f7cc57adecfc5
Alex Messina <atm1984@gmail.com>
2016-05-25 12:19:07 -0700

    upgraded to statsmodels 0.7.0 developer and patsy 0.4.1
    patsy 0.4.1 was supposed to resolve the issue:
    num_columns must be an int

--- settings/.spyder/history.py
+++ settings/.spyder/history.py
@@ -15,3 +15,36 @@ runfile('C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code/SedFl
 ##---(Thu Jun 09 10:53:21 2016)---
 print 'helloworld'
 runfile('C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code/SedFlux.py', wdir='C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code')
+
+##---(Wed May 25 11:55:28 2016)---
+runfile('C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code/Load_SedPod_Tube_Data.py', wdir='C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code')
+import patsy
+patsy.__version__
+patsy.__path__
+import numpy
+numpy.__version__
+import statsmodels
+statsmodels.__version__
+
+##---(Wed May 25 12:00:30 2016)---
+import numpy as np
+type(np.zeros(10).shape[0])
+import pandas
+pandas.__version__
+
+##---(Wed May 25 12:06:18 2016)---
+import statsmodels
+statsmodels.__version__
+runfile('C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code/Load_SedPod_Tube_Data.py', wdir='C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code')
+
+##---(Wed May 25 12:07:43 2016)---
+import patsy
+patsy.__version__
+runfile('C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code/Load_SedPod_Tube_Data.py', wdir='C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code')
+
+##---(Wed May 25 12:10:24 2016)---
+import patsy
+patsy.__version__
+import statsmodels
+statsmodels.__version__
+runfile('C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code/Load_SedPod_Tube_Data.py', wdir='C:/Users/atm19/Documents/GitHub/Fagaalu-Sedimentation/Code')
