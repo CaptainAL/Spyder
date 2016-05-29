0f07e6a7381794842494f73cf5babc8c8136496d
Alex Messina <atm1984@gmail.com>
2016-05-28 17:24:58 -0700

    rolled back Pandas to version 0.13.0
    version 17 wasn't working with my slicing

---
+++ USING PYTHON README.txt
@@ -0,0 +1,9 @@
+First install the version of R in R_install_executable if you don't have R already
+
+Open up WinPython Control Panel.exe
+
+Go to Advanced > Register Distribution (click "yes")
+
+Open up "Spyder.exe"
+
+Do good stuff.
--- settings/.spyder/history.py
+++ settings/.spyder/history.py
@@ -3,3 +3,11 @@
 
 ##---(Tue May 10 17:36:44 2016)---
 print 'hello world'
+
+##---(Sat May 28 17:06:35 2016)---
+import pandas
+pandas.__version__
+
+##---(Sat May 28 17:09:54 2016)---
+runfile('C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code/SedFlux.py', wdir='C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code')
+runfile('C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code/SedFlux.py', wdir='C:/Users/Alex Messina/Documents/GitHub/Fagaalu-Sediment-Flux/Code')
